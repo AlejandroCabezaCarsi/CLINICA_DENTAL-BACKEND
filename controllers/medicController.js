ac6a643922b25abfab10c285685d6e669b9a5dc2
Alex <cabezacarsialejandro@gmail.com>
2023-06-12 18:50:37 +0200

    [ADD] Created loginMedic, working correctly

--- controllers/medicController.js
+++ controllers/medicController.js
@@ -4,6 +4,9 @@ const medicController = {}
 
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+
+
 medicController.createMedic = async (req, res) => {
     try {
 
@@ -36,6 +39,57 @@ medicController.createMedic = async (req, res) => {
   };
 
 
-
+  medicController.loginMedic = async (req, res) => {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
+  
+      const buscaMedico = await medic.findOne({
+        where: {
+          email: email,
+        },
+      });
+  
+      if (!buscaMedico) {
+        return res.json({
+          success: true,
+          message: "Wrong credentials",
+        });
+      }
+  
+      const validPassword = bcrypt.compareSync(password, buscaMedico.password);
+  
+      if (!validPassword) {
+        return res.json({
+          success: true,
+          message: "Wrong credentials",
+        });
+      }
+  
+      const token = jwt.sign(
+        {
+          userId: medic.id,
+          roleId: medic.roleId,
+          email: medic.email,
+        },
+        "zumitoDePiña",
+        {
+          expiresIn: "8h",
+        }
+      );
+  
+      return res.json({
+        success: true,
+        message: "User Logged",
+        token: token,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: true,
+        message: "Wrong credentials",
+        error: error.message,
+      });
+    }
+  };
 
 module.exports = medicController
--- views/medicRouter.js
+++ views/medicRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); 
 const medicController = require("../controllers/medicController");
 
-router.post('/create', medicController.createMedic)
+router.post('/create', medicController.createMedic);
+router.post('/login', medicController.loginMedic);
+
 
 
 module.exports = router
