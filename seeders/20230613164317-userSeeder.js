6599c87c24f1eecf9b39124ae43daf544ef17231
Alex <cabezacarsialejandro@gmail.com>
2023-06-13 19:00:03 +0200

    [CHANGE] Changed the seeder of user and deleted appointment seeder

--- seeders/20230613164317-userSeeder.js
+++ seeders/20230613164317-userSeeder.js
@@ -1,10 +1,7 @@
-"use strict";
+'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-
-
-
   async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
@@ -24,9 +21,10 @@ module.exports = {
         lastname: "superAdmin",
         email: "superAdmin@superAdmin.com",
         password: bcrypt.hashSync("1234", 8),
-        phonenumber: 111111111,
         dni: "123456789A",
+        phoneNumber: 111111111,
         roleId: 1,
+        isActive:true,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -35,44 +33,51 @@ module.exports = {
         name: "admin",
         lastname: "admin",
         email: "admin@admin.com",
-        password :bcrypt.hashSync("1234", 8),
-        phonenumber: 222222222,
+        password: bcrypt.hashSync("1234", 8),
         dni: "987654321B",
+        phoneNumber: 222222222,
         roleId: 2,
+        isActive:true,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id:3,
         name: "medic",
         lastname: "medic",
         email: "medic@medic.com",
         password: bcrypt.hashSync("1234", 8),
-        phonenumber: 333333333,
         dni: "67891234C",
+        phoneNumber: 333333333,
         roleId: 3,
+        isActive:true,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id:4,
         name: "patient",
         lastname: "patient",
         email: "patient@patient.com",
         password: bcrypt.hashSync("1234", 8),
-        phonenumber: 333333333,
-        dni: "67891234C",
+        dni: "67891234D",
+        phoneNumber: 444444,
         roleId: 4,
+        isActive:true,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     ]);
   },
 
-  async down(queryInterface, Sequelize) {
+
+
+  async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  },
+  }
 };
